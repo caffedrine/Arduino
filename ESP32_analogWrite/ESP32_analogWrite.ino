1ea8cc7c4fab5503fdaba5a7dca8665fe3b1ac5a
caffedrine <bacchusstyle@yahoo.com>
2017-05-19 17:37:55 +0200

    Updates

--- ESP32_analogWrite/ESP32_analogWrite.ino
+++ ESP32_analogWrite/ESP32_analogWrite.ino
@@ -1,12 +1,39 @@
 #include "Arduino.h"
-//The setup function is called once at startup of the sketch
+
+#include <esp32-hal-dac.h>
+#include "my_util.h"
+
+int dac1Pin = 25;
+int dac2Pin = 26;
+
+int analogInputPin = 36;
+
 void setup()
 {
-// Add your initialization code here
+	Serial.begin(115200);
+	pinMode(dac1Pin, OUTPUT);
+	pinMode(dac2Pin, OUTPUT);
+	pinMode(analogInputPin, INPUT);
 }
 
-// The loop function is called in an endless loop
+int writtenVal = 0;
+int readedVal = 0;
+
 void loop()
 {
-//Add your repeated code here
+	if(Serial.available())
+	{
+		writtenVal = to_int(Serial.readString());
+	}
+
+	dacWrite(25, writtenVal);//pin, 8bit value
+	dacWrite(26, writtenVal);//pin, 8bit value
+	delay(1);
+
+	//We want to make sure about value we write, so we read the value using another port
+	readedVal = analogRead(analogInputPin);
+	delay(1);
+
+	//Print data for debugging
+	printPeriodicData("Current speed: " + to_string(writtenVal) + "\t Readed val: " + readedVal, 1000);
 }
