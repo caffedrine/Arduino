efd707c8c69a8997682847e72b2509a69a10cadd
caffedrine <bacchusstyle@yahoo.com>
2017-05-21 22:43:38 +0200

    just in case

--- QRE1113_AnalogLineSensor/QRE1113_AnalogLineSensor.ino
+++ QRE1113_AnalogLineSensor/QRE1113_AnalogLineSensor.ino
@@ -1,75 +1,23 @@
 #include <Arduino.h>
+#include "QRE1113_Driver.h"
 
-#include "my_util.h"
-
-int encoderLeftPin = 35;
-int encoderRightPin = 39;
-
-int leftVal  = 0, lastLeftVal   = 0, leftSteps  = 0;
-int rightVal = 0, lastRightVal  = 0, rightSteps = 0;
-
-//Prototypes
-void printPeriodicData(String, int);
+QRE1113 encoder(35);
 
 void setup()
 {
-	pinMode(encoderLeftPin, INPUT);
-	pinMode(encoderRightPin, INPUT);
 	Serial.begin(115200);
 	Serial.println("QRE1113 Reflectation Sensors");
 	Serial.println("------------------------");
 }
 
+
 void loop()
 {
-	//Print only when value is changed
-	static int lastLeftSteps = 0;
-	static int lastRightSteps = 0;
-	lastLeftSteps = leftSteps;
-	lastRightSteps = rightSteps;
-
-	lastLeftVal  = leftVal;
-	lastRightVal = rightVal;
-
-	leftVal  = analogRead(encoderLeftPin);
-	rightVal = analogRead(encoderRightPin);
-
-	if(leftVal < 500)
-		leftVal = 1;
-	else if(leftVal > 2150)
-		leftVal = 0;
-	else
-		leftVal = lastLeftVal;
-
-	if(rightVal < 500)
-		rightVal = 1;
-	else if(rightVal > 2150)
-		rightVal = 0;
-	else
-		rightVal = lastRightVal;
-
-	if(lastLeftVal != leftVal)
-		leftSteps += leftVal;
-
-	if(lastRightVal != rightVal)
-		rightSteps += rightVal;
-
-	if(leftSteps != lastLeftSteps || rightSteps != lastRightSteps)
+	//Updating sensors data
+	encoder.update();
+	if(encoder.lastSteps != encoder.currSteps)
 	{
-		Serial.println("Steps:   " + to_string(leftSteps) + "\t" + to_string(rightSteps));
-	}
-
-
-	//printPeriodicData("Steps:   " + to_string(leftSteps) + "\t" + to_string(rightSteps), 10);/*
-	//printPeriodicData("Steps:   " + to_string(leftVal) + "\t" + to_string(rightVal), 100);//*/
-}
-
-void printPeriodicData(String data, int interval = 1000)
-{
-	static long previousMillis = 0;
-	if (millis() - previousMillis > interval)
-	{
-		previousMillis = millis();
-		Serial.println(data);
+		Serial.println("Steps: " + String(encoder.getSteps()));
 	}
 }
+
--- QRE1113_AnalogLineSensor/my_util.h
+++
@@ -1,7 +0,0 @@
-#include <WString.h>
-#include <Arduino.h>
-
-String to_string(int intNumber);
-int to_int(String str);
-int getNumberOfChars(String str, char checkCharacter);
-String getStringPartByNr(String data, char separator, int index);
