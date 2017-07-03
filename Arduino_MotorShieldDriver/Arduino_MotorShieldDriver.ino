96158ede53aa1e1e335056f2aacb85493a6f5d7d
caffedrine <bacchusstyle@yahoo.com>
2017-07-03 19:10:43 +0200

    Motor shield driver

--- Arduino_MotorShieldDriver/Arduino_MotorShieldDriver.ino
+++ Arduino_MotorShieldDriver/Arduino_MotorShieldDriver.ino
@@ -1,12 +1,42 @@
 #include "Arduino.h"
-//The setup function is called once at startup of the sketch
+
+#include "DCMotor.h"
+#include "my_util.h"
+
+//Motors
+DCMotor M1(3, 12);
+DCMotor M2(11, 13);
+
 void setup()
 {
-// Add your initialization code here
+	//Start serial - we need to get speed
+	Serial.begin(115200);
+	Serial.println("---STARTED---");
+
+	//Make sure motors are stopped
+	M1.brake();
+	M2.brake();
+
+	//Set directions
+	M1.setDirection(DCMotor::DIRECTION_FORWARD);
+	M2.setDirection(DCMotor::DIRECTION_FORWARD);
 }
 
-// The loop function is called in an endless loop
+int speed = 0;
 void loop()
 {
-//Add your repeated code here
+	//Run Forest, run!
+	M1.run();
+	M2.run();
+
+	//Update motors speeds from serial
+	if(Serial.available())
+	{
+		speed = to_int(Serial.readString());
+		M1.setSpeed(speed);
+		M2.setSpeed(speed);
+	}
+
+	//Print speed every 1 seconds
+	printPeriodicData("Speed: " + to_string(speed), 1000);
 }
