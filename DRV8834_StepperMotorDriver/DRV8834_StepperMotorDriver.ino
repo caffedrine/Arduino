09800815381cd879141b9c4f4abbc492b1cbc4a3
curiosul <bacchusstyle@yahoo.com>
2017-07-11 16:50:07 +0200

    Motor changes

--- DRV8834_StepperMotorDriver/DRV8834_StepperMotorDriver.ino
+++ DRV8834_StepperMotorDriver/DRV8834_StepperMotorDriver.ino
@@ -1,97 +1,34 @@
-<<<<<<< HEAD
-/**
-	DRV8834 Steper Motor - Driver Library
-	Source: https://github.com/laurb9/StepperDriver/blob/master/examples/ClockStepper/ClockStepper.ino
-*/
-#include "DRV8834.h"
-
-//Pins definitions
-#define MOTOR_STEPS	200// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
-#define	DIR		8	//direction pin
-#define	STEP	9	//step pin
-#define MS1		10	//microstep
-#define MS2		11	//microstep
-#define MS3		0	//microstep
-
-DRV8834 motor(MOTOR_STEPS, DIR, STEP);
-
-void setup()
-{
-	Serial.begin(115200);
-	Serial.println("---SRART---");
-
-	motor.setRPM(1);
-	// Set full speed mode (microstepping also works for smoother hand movement
-	motor.setMicrostep(1);
-=======
-/*
- * Simple demo, should work with any driver board
- *
- * Connect STEP, DIR as indicated
- *
- * Copyright (C)2015 Laurentiu Badea
- *
- * This file may be redistributed under the terms of the MIT license.
- * A copy of this license has been included with this distribution in the file LICENSE.
- */
 #include <Arduino.h>
-#include "BasicStepperDriver.h"
+#include "DRV8834.h"
+#include "../libs/my_util.h"
 
 // Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
 #define MOTOR_STEPS 200
 
+//enable pin
+#define ENBL 13
+
 // All the wires needed for full functionality
 #define DIR 8
 #define STEP 9
-//Uncomment line to use enable/disable functionality
-//#define ENBL 7
 
-// Since microstepping is set externally, make sure this matches the selected mode
-// 1=full step, 2=half step etc.
-#define MICROSTEPS 1
+// microstep control for DRV8834
+#define M0 10
+#define M1 11
+DRV8834 stepper(MOTOR_STEPS, DIR, STEP, M0, M1);
 
-// 2-wire basic config, microstepping is hardwired on the driver
-BasicStepperDriver stepper(MOTOR_STEPS, DIR, STEP);
-
-//Uncomment line to use enable/disable functionality
-//BasicStepperDriver stepper(MOTOR_STEPS, DIR, STEP, ENBL);
-
-void setup()
+void setup() 
 {
 	/*
-	 * Set target motor RPM.
-	 * These motors can do up to about 200rpm.
-	 * Too high will result in a high pitched whine and the motor does not move.
-	 *
-	 * Also tell the driver the microstep level we selected.
-	 * If mismatched, the motor will move at a different RPM than chosen.
-	 */
-	stepper.begin(120, MICROSTEPS);
->>>>>>> origin/master
+	* Set target motor RPM=1 and microstepping=1
+	*/
+	stepper.begin(1, 1);
 }
 
-void loop()
+void loop() 
 {
-<<<<<<< HEAD
-	motor.rotate(360);
-=======
-
-	// energize coils - the motor will hold position
-	// stepper.enable();
-
 	/*
-	 * Moving motor one full revolution using the degree notation
-	 */
+	* The easy way is just tell the motor to rotate 360 degrees at 1rpm
+	*/
 	stepper.rotate(360);
-
-	/*
-	 * Moving motor to original position using steps
-	 */
-	stepper.move(-200 * MICROSTEPS);
-
-	// pause and allow the motor to be moved by hand
-	// stepper.disable();
-
-	delay(5000);
->>>>>>> origin/master
 }
