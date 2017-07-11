237876fb436a4116bd612b82c4bf65607a21bcef
curiosul <bacchusstyle@yahoo.com>
2017-07-11 16:33:11 +0200

    Commit
    1223

--- DRV8834_StepperMotorDriver/DRV8834_StepperMotorDriver.ino
+++ DRV8834_StepperMotorDriver/DRV8834_StepperMotorDriver.ino
@@ -1,17 +1,30 @@
 /**
 	DRV8834 Steper Motor - Driver Library
+	Source: https://github.com/laurb9/StepperDriver/blob/master/examples/ClockStepper/ClockStepper.ino
 */
+#include "DRV8834.h"
+
+//Pins definitions
+#define MOTOR_STEPS	200// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
+#define	DIR		8	//direction pin
+#define	STEP	9	//step pin
+#define MS1		10	//microstep
+#define MS2		11	//microstep
+#define MS3		0	//microstep
+
+DRV8834 motor(MOTOR_STEPS, DIR, STEP);
 
 void setup()
 {
+	Serial.begin(115200);
+	Serial.println("---SRART---");
 
-  /* add setup code here */
-
+	motor.setRPM(1);
+	// Set full speed mode (microstepping also works for smoother hand movement
+	motor.setMicrostep(1);
 }
 
 void loop()
 {
-
-  /* add main program code here */
-
+	motor.rotate(360);
 }
