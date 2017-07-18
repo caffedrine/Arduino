89c54b6c835918f83c7db177ffe433b4dddca250
caffedine <bacchusstyle@yahoo.com>
2017-07-18 15:13:13 +0200

    Non blocking driver for stepper

--- A4988_StepperNonBlockingLoop/A4988_StepperNonBlockingLoop.ino
+++ A4988_StepperNonBlockingLoop/A4988_StepperNonBlockingLoop.ino
@@ -3,32 +3,85 @@
  */
 
 #include <Arduino.h>
+#include "../libs/my_util.h"
 
-// defines pins numbers
-const int dirPin = 8;
-const int stepPin = 9;
+// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
+#define MOTOR_STEPS 200
+
+// All the wires needed for full functionality
+#define DIR 8
+#define STEP 9
+#define ENBL 13
+
+// microsteps pins
+#define MS1 10
+#define MS2 11
+#define MS3 12
 
 void setup()
 {
+	//Start serial comm
+	Serial.begin(115200);
+
 	// Sets the two pins as Outputs
-	pinMode(stepPin, OUTPUT);
-	pinMode(dirPin, OUTPUT);
+	pinMode(DIR, OUTPUT);
+	pinMode(STEP, OUTPUT);
+	pinMode(ENBL, OUTPUT);
+	pinMode(MS1, OUTPUT);
+	pinMode(MS2, OUTPUT);
+	pinMode(MS3, OUTPUT);
 
 	//predefine direction
-	digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
+	digitalWrite(DIR, HIGH); // Enables the motor to move in a particular direction
+
+	//enable microsteping 16
+	digitalWrite(MS1, HIGH);
+	digitalWrite(MS2, HIGH);
+	digitalWrite(MS3, HIGH);
 }
 
-int pause = 800;	//vaqry speed from here
+unsigned int pause = 100;	//microseconds
 void loop()
 {
 
-	// Makes 200 pulses for making one full cycle rotation
-	for (int x = 0; x < 200; x++)
+	/*
+	// Makes 200 pulses*microsteps for making one full cycle rotation
+	for (int x = 0; x < 200*16; x++)
 	{
-		digitalWrite(stepPin, HIGH);
+		digitalWrite(STEP, HIGH);
 		delayMicroseconds(pause);
-		digitalWrite(stepPin, LOW);
+		digitalWrite(STEP, LOW);
 		delayMicroseconds(pause);
 	}
 	delay(1000); // One second delay;
+	//*/
+
+	if(Serial.available())
+	{
+		int readVal = to_int ( Serial.readString() );
+		if(readVal == 0)
+		{
+			pause = 200;
+			digitalWrite(ENBL, HIGH);
+			Serial.println("BRAKE!");
+		}
+		else
+		{
+			pause = readVal;
+			digitalWrite(ENBL, LOW);
+			Serial.println("NEW SPEED: " + to_string(pause));
+		}
+	}
+
+	//Update motor every interval of time
+	static unsigned int prevMicros = 0;
+	static bool lastState = 0;
+	if(micros() - prevMicros > pause)
+	{
+		digitalWrite(STEP, !lastState);
+
+		lastState = !lastState;
+		prevMicros = micros();
+	}
+	//*/
 }
