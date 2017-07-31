e1576f4896b1bf6d7ee897552cf7efcb501ed5d6
caffedrine <bacchusstyle@yahoo.com>
2017-07-31 12:53:57 +0200

    DRV8834_Driver

---
+++ DRV8834_StepperMotorNonblockingLoop/DRV8834_StepperMotorNonblockingLoop.ino
@@ -0,0 +1,62 @@
+/*     Simple Stepper Motor Control
+ *	   -- NON-BLOCKING LOOP --
+ */
+
+#include <Arduino.h>
+#include "../libs/my_util.h"
+#include "StepperPWM.h"
+
+// All the wires needed for full functionality
+#define DIR 8
+#define STEP 9
+#define ENBL 13
+
+// microsteps pins
+#define MS1 10
+#define MS2 11
+#define MS3 12
+
+StepperPWM stepper(DIR, STEP, ENBL, MS1, MS2, MS3);
+
+void setup()
+{
+	//Start serial communication
+	Serial.begin(115200);
+	Serial.println("---START---");
+
+	//Init motor
+	if(stepper.init())
+	{
+		Serial.println("Motor initialized!");
+		stepper.brake();
+	}
+	else
+		Serial.println("ERROR: Can't initialize motor with given pins!");
+
+	stepper.enable();
+	stepper.setFrequency(2000);
+}
+
+unsigned long speed = 200;	//microseconds
+void loop()
+{
+	if(Serial.available())
+	{
+		int readVal = to_int ( Serial.readString() );
+		if(readVal == 0)
+		{
+			speed = 200;
+			stepper.brake();
+			Serial.println("BRAKE!");
+		}
+		else
+		{
+			speed = readVal;
+			stepper.setFrequency(speed);
+			stepper.enable();
+			stepper.run();	//Write val right away ^_^
+			Serial.println("NEW SPEED: " + to_string(speed));
+		}
+	}
+	stepper.run();
+}
