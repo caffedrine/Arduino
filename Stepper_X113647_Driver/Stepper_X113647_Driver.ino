8477bf3f2e029d6abefeb066c9c773296b7182cd
caffedrine <bacchusstyle@yahoo.com>
2019-04-16 00:07:27 +0300

    x113647 motor driver class

--- Stepper_X113647_Driver/Stepper_X113647_Driver.ino
+++ Stepper_X113647_Driver/Stepper_X113647_Driver.ino
@@ -1,72 +1,15 @@
 #include "Arduino.h"
 
-//#include "X113647Stepper.h"
-//
-//tardate::X113647Stepper motor(20000, 8, 9, 10, 11, tardate::StepMode::SingleStep);
+#include "X113647Stepper.h"
 
-class X11Stepper
-{
-public:
-	enum class DIRECTION
-	{
-		FORWARD = 0,
-		BACKWARD = 1,
-	};
-
-	X11Stepper(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4) : _pinIn1(in1), _pinIn2(in2), _pinIn3(in3), _pinIn4(in4)
-	{
-		pinMode(this->_pinIn1, OUTPUT);
-		pinMode(this->_pinIn2, OUTPUT);
-		pinMode(this->_pinIn3, OUTPUT);
-		pinMode(this->_pinIn4, OUTPUT);
-	}
-
-	void StepNext()
-	{
-		if(this->_CurrentDirection == DIRECTION::FORWARD)
-		{
-			if( (this->_CurrentStep++)>=(sizeof(this->STEPS)-1))
-				this->_CurrentStep = 0;
-		}
-		else if(this->_CurrentDirection == DIRECTION::BACKWARD)
-		{
-			if(this->_CurrentStep == 0)
-				this->_CurrentStep = sizeof(this->STEPS)-1;
-			else
-				this->_CurrentStep--;
-		}
-		this->_Step(this->_CurrentStep);
-	}
-
-	void SetDirection(DIRECTION dir)
-	{
-		this->_CurrentDirection = dir;
-	}
-
-private:
-	const uint8_t STEPS[4] = {0b00001100, 0b00000110, 0b00000011, 0b00001001};
-
-	uint8_t _pinIn1, _pinIn2, _pinIn3, _pinIn4;
-	uint8_t _CurrentStep = 0;
-	DIRECTION _CurrentDirection = DIRECTION::FORWARD;
-
-	void _Step(uint8_t STEP_NO)
-	{
-		digitalWrite( this->_pinIn1, 0b00001000 & STEPS[STEP_NO] );
-		digitalWrite( this->_pinIn2, 0b00000100 & STEPS[STEP_NO] );
-		digitalWrite( this->_pinIn3, 0b00000010 & STEPS[STEP_NO] );
-		digitalWrite( this->_pinIn4, 0b00000001 & STEPS[STEP_NO] );
-	}
-};
-
-X11Stepper motor(8, 9, 10, 11);
+X11Stepper motor(40, 41, 42, 43);
 
 
 void setup()
 {
 	Serial.begin(9600);
 
-	motor.SetDirection(X11Stepper::DIRECTION::BACKWARD);
+	motor.SetDirection(X11Stepper::DIRECTION::FORWARD);
 
 }
 
--- libs/X113647Stepper/X113647Stepper.h
+++ libs/X113647Stepper/X113647Stepper.h
@@ -1,89 +1,65 @@
-#pragma once
-/*
-  X113647Stepper - a X113647/ULN2003A Stepper Motor library for Arduino
+/**
+ * Author: curiosul
+ */
 
-  This library is derived from the Arduino Stepper library http://www.arduino.cc/en/Tutorial/Stepper
-  but with two differences:
+#ifndef _X113647Stepper_h_
+#define _X113647Stepper_h_
 
-  * uses a default pin mapping that matches the 28BYJ-48 stepper motor (1-3-2-4 compared to the standard library).
-  * supports full-step, half-step and single-step modes
+#include "Arduino.h"
 
-  For info and circuit diagrams see https://github.com/tardate/X113647Stepper
+class X11Stepper
+{
+public:
+	enum class DIRECTION
+	{
+		FORWARD = 0,
+		BACKWARD = 1,
+	};
 
-*/
+	X11Stepper(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4) : _pinIn1(in1), _pinIn2(in2), _pinIn3(in3), _pinIn4(in4)
+	{
+		pinMode(this->_pinIn1, OUTPUT);
+		pinMode(this->_pinIn2, OUTPUT);
+		pinMode(this->_pinIn3, OUTPUT);
+		pinMode(this->_pinIn4, OUTPUT);
+	}
 
+	void StepNext()
+	{
+		if(this->_CurrentDirection == DIRECTION::FORWARD)
+		{
+			if( (this->_CurrentStep++)>=(sizeof(this->STEPS)-1))
+				this->_CurrentStep = 0;
+		}
+		else if(this->_CurrentDirection == DIRECTION::BACKWARD)
+		{
+			if(this->_CurrentStep == 0)
+				this->_CurrentStep = sizeof(this->STEPS)-1;
+			else
+				this->_CurrentStep--;
+		}
+		this->_Step(this->_CurrentStep);
+	}
 
-namespace tardate {
+	void SetDirection(DIRECTION dir)
+	{
+		this->_CurrentDirection = dir;
+	}
 
-/// Define the step mode
-///
-enum class StepMode : uint8_t {
-    SingleStep,
-    FullStep,
-    HalfStep
+private:
+	const uint8_t STEPS[4] = {0b00001100, 0b00000110, 0b00000011, 0b00001001};
+
+	uint8_t _pinIn1, _pinIn2, _pinIn3, _pinIn4;
+	uint8_t _CurrentStep = 0;
+	DIRECTION _CurrentDirection = DIRECTION::FORWARD;
+
+	void _Step(uint8_t STEP_NO)
+	{
+		digitalWrite( this->_pinIn1, 0b00001000 & STEPS[STEP_NO] );
+		digitalWrite( this->_pinIn2, 0b00000100 & STEPS[STEP_NO] );
+		digitalWrite( this->_pinIn3, 0b00000010 & STEPS[STEP_NO] );
+		digitalWrite( this->_pinIn4, 0b00000001 & STEPS[STEP_NO] );
+	}
 };
 
-
-class X113647Stepper {
-  public:
-
-    /*
-     * Constructor with step mode specification
-     * Sets the number of steps per revolution, stepping mode and which pins should control the motor.
-     */
-    X113647Stepper(int steps_per_revolution, int pin_in1, int pin_in2, int pin_in3, int pin_in4, StepMode step_mode = StepMode::FullStep);
-
-    /*
-     * Command: sets the speed in revs per minute
-     */
-    void setSpeed(float whatSpeed);
-
-    /*
-     * Command: Moves the motor steps_to_move steps.
-     * If the number is negative, the motor moves in the reverse direction.
-     */
-    void step(int number_of_steps);
-
-    /*
-     * Command: Emergency Stop.
-     * Terminates any movement in progres.
-     */
-    void emergencyStop();
-
-    /*
-     * Returns the version of the library
-     */
-    int version(void);
-
-  private:
-
-    /*
-     * Command: initialise stepper output
-     */
-    void ignition();
-
-    /*
-     * Command: moves the motor forward or backwards.
-     */
-    void stepMotor(int this_step);
-
-    unsigned long step_delay;     // delay between steps, in ms, based on speed
-    unsigned long minimum_delay;  // minimum delay between steps
-    int steps_per_revolution;     // actual number of steps per revolution given step mode
-    StepMode step_mode;           // number of phases being used to drive the motor
-    int steps_per_cycle;          // number of steps per revolution (before gearing)
-    int signals_per_step;         // number of signals per step. when full step == 1, when half-step == 2
-    int step_number;              // which step the motor is on
-    volatile int steps_remaining; // number of steps remaining in step()
-
-    // motor pin numbers:
-    int pin_in1;
-    int pin_in2;
-    int pin_in3;
-    int pin_in4;
-
-    int step_codes[8];
-
-};
-
-}
+#endif
