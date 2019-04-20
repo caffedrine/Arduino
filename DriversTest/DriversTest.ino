eeafe8c15821f19894a218bd44341781207f5a6c
caffedrine <bacchusstyle@yahoo.com>
2019-04-20 09:02:46 +0300

    finished hc595 test

--- DriversTest/DriversTest.ino
+++ DriversTest/DriversTest.ino
@@ -11,17 +11,19 @@ HC595 reg(2, 3, 4, 2);
 void setup()
 {
 	Serial.begin(115200);
-	reg.ClearAll();
+
+	/* Set initial states */
+	uint8_t data[2];
+	data[0] = 0b01010101;
+	data[1] = 0b10101010;
+	reg.WriteRaw(data, 2);
 }
 
 void loop()
 {
 	led.Toggle();
 
-	uint8_t data[2];
-	data[0] = 0b01010101;
-	data[1] = 0b10101010;
-	reg.WriteRaw(data, 2);
+	reg.ToggleAll();
 	reg.Update();
 
 	delay(50);
